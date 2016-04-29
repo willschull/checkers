40000094f5a99cc981ea02de6b5e8a149810c78f
Will Schull <wschull@asu.edu>
2016-04-29 11:37:19 -0400

    Changed text of the pieces if player picks red

--- Checkers/src/checkers/Checkers.java
+++ Checkers/src/checkers/Checkers.java
@@ -37,12 +37,12 @@ public class Checkers extends JPanel implements ActionListener, ItemListener, Mo
     JLabel diff=new JLabel("Difficulty Level");
     JLabel rp=new JLabel();
     JLabel rpt=new JLabel("Opponent's Piece");
-    JLabel bpt=new JLabel("Your Piece");
-    JLabel bp=new JLabel();
+    JLabel ypt=new JLabel("Your Piece");
+    JLabel yp=new JLabel();
     JLabel rk=new JLabel();
     JLabel rkt=new JLabel("Opponent's King");
-    JLabel bkt=new JLabel("Your King");
-    JLabel bk=new JLabel();
+    JLabel ykt=new JLabel("Your King");
+    JLabel yk=new JLabel();
 
     JComboBox level=new JComboBox();
 
@@ -85,9 +85,11 @@ public class Checkers extends JPanel implements ActionListener, ItemListener, Mo
 
     Checkers(){
         setupGUI();
+        
     }
 
     private void setupGUI(){
+    	
         setLayout(null);
 
         newButton.setFocusPainted(false);
@@ -143,17 +145,17 @@ public class Checkers extends JPanel implements ActionListener, ItemListener, Mo
         this.add(p1);
         this.add(p2);
 
-        col.setBounds(410,400,80,25);
+        col.setBounds(420,340,80,25);
         //was commented below
-        //this.add(col);
+        this.add(col);
         c1.addActionListener(this);
         c2.addActionListener(this);
         c1.setCursor(new Cursor(Cursor.HAND_CURSOR));
         c2.setCursor(new Cursor(Cursor.HAND_CURSOR));
         colors.add(c1);
         colors.add(c2);
-        c1.setBounds(415,420,80,25); //90,440,80,25
-        c2.setBounds(415,440,80,25); //90,420,80,25
+        c1.setBounds(415,370,80,25);//90,440,80,25
+        c2.setBounds(415,398,80,25); //90,420,80,25
         //2 below were commented
         this.add(c1);
         this.add(c2);
@@ -186,11 +188,11 @@ public class Checkers extends JPanel implements ActionListener, ItemListener, Mo
         rpt.setBounds(60, 450, 60, 20);
         this.add(rpt);
 
-        bp.setBounds(110, 440, 50, 50);
-        bp.setIcon(yellowN);
-        this.add(bp);
-        bpt.setBounds(160, 450, 90, 20);
-        this.add(bpt);
+        yp.setBounds(110, 440, 50, 50);
+        yp.setIcon(yellowN);
+        this.add(yp);
+        ypt.setBounds(160, 450, 90, 20);
+        this.add(ypt);
 
         rk.setBounds(250, 440, 50, 50);
         rk.setIcon(redK);
@@ -198,18 +200,20 @@ public class Checkers extends JPanel implements ActionListener, ItemListener, Mo
         rkt.setBounds(305, 450, 60, 20);
         this.add(rkt);
 
-        bk.setBounds(365, 440, 50, 50);
-        bk.setIcon(yellowK);
-        this.add(bk);
-        bkt.setBounds(420, 450, 100, 20);
-        this.add(bkt);
+        yk.setBounds(365, 440, 50, 50);
+        yk.setIcon(yellowK);
+        this.add(yk);
+        ykt.setBounds(420, 450, 100, 20);
+        this.add(ykt);
         //2 below were commented
         //g=getGraphics();
-        //g.drawImage(redN.getImage(),30,450,this);
+        //g.drawImage(redN.getImage(),500,500,this); //redN.getImage(),30,450,this
+       
     }
 
     public void paintComponent(Graphics g)	{
 		super.paintComponent(g);
+		
         g.setColor(new Color(0,0,0));
 
         for(int i=0;i<4;i++){
@@ -287,11 +291,12 @@ public class Checkers extends JPanel implements ActionListener, ItemListener, Mo
     }
 
     public void newGame()	{                            //creates a new game
-    	//TEST
+    
         //Yellow takes the first move in both modes
         //If someone wants to move secondly, red has to be selected
         //Yellow is always at the bottom of the board
 
+    	
         selectedColor= c1.isSelected() ? "red" : "yellow";
         selectedMode=p1.isSelected()?1:2;
         difficulty=level.getSelectedIndex();
@@ -331,14 +336,24 @@ public class Checkers extends JPanel implements ActionListener, ItemListener, Mo
             preToMove3=preToMove2=preToMove1=toMove;
         }
 
+        
+        
         if (selectedMode == 1 && selectedColor.equalsIgnoreCase("yellow"))
 		{
+        	rpt.setText("Opponent's Piece");
+ 		   	ypt.setText("Your Piece");
+ 		   	rkt.setText("Opponent's King");
+ 		   	ykt.setText("Your Piece");
             this.toMove = redNormal;
             play();
 		}
 		else if (selectedMode==1 && selectedColor.equalsIgnoreCase("red"))
 		{
-           this.toMove = redNormal;
+			rpt.setText("Your Piece");
+		   	ypt.setText("Opponent's Piece");
+		   	rkt.setText("Your King");
+		   	ykt.setText("Opponent's Piece");
+		   	this.toMove = redNormal;
             play();
 		}
 
